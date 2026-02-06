dec118bfd1c9f0caa8226b46f33cda7353f4bb8b
Sariaka Razakandisa <sariakaraz8@gmail.com>
2026-02-06 15:30:42 +0300

    fix Reservation : objet Hotel, et Timestamp dateHeureArrivee

--- carlocation/src/main/java/com/example/entity/Reservation.java
+++ carlocation/src/main/java/com/example/entity/Reservation.java
@@ -2,7 +2,6 @@ package com.example.entity;
 
 import com.example.config.DbConnection;
 import java.sql.*;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,28 +9,23 @@ public class Reservation {
 
     private Integer idReservation;
     private Integer nbPassager;
-    private LocalDateTime dateHeureArrivee;
-    private Integer idHotel;
+    private Timestamp dateHeureArrivee;
+    private Hotel hotel;
     private String idClient;
 
-    public Reservation() {
-    }
+    public Reservation() {}
 
-    public Reservation(Integer idReservation, Integer nbPassager, LocalDateTime dateHeureArrivee,
-                       Integer idHotel, String idClient) {
+    public Reservation(Integer idReservation, Integer nbPassager,
+                       Timestamp dateHeureArrivee, Hotel hotel, String idClient) {
         this.idReservation = idReservation;
         this.nbPassager = nbPassager;
         this.dateHeureArrivee = dateHeureArrivee;
-        this.idHotel = idHotel;
+        this.hotel = hotel;
         this.idClient = idClient;
     }
 
     public Integer getIdReservation() {
         return idReservation;
-    }
-
-    public void setIdReservation(Integer idReservation) {
-        this.idReservation = idReservation;
     }
 
     public Integer getNbPassager() {
@@ -42,20 +36,20 @@ public class Reservation {
         this.nbPassager = nbPassager;
     }
 
-    public LocalDateTime getDateHeureArrivee() {
+    public Timestamp getDateHeureArrivee() {
         return dateHeureArrivee;
     }
 
-    public void setDateHeureArrivee(LocalDateTime dateHeureArrivee) {
+    public void setDateHeureArrivee(Timestamp dateHeureArrivee) {
         this.dateHeureArrivee = dateHeureArrivee;
     }
 
-    public Integer getIdHotel() {
-        return idHotel;
+    public Hotel getHotel() {
+        return hotel;
     }
 
-    public void setIdHotel(Integer idHotel) {
-        this.idHotel = idHotel;
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
     }
 
     public String getIdClient() {
@@ -67,45 +61,58 @@ public class Reservation {
     }
 
     public void save() throws SQLException {
-        String sql = "INSERT INTO reservation (nb_passager, date_heure_arrivee, id_hotel, id_client) " +
-                     "VALUES (?, ?, ?, ?)";
+        String sql = """
+            INSERT INTO reservation (nb_passager, date_heure_arrivee, id_hotel, id_client)
+            VALUES (?, ?, ?, ?)
+        """;
 
         try (Connection conn = DbConnection.getInstance().getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
 
-            stmt.setInt(1, this.nbPassager);
-            stmt.setTimestamp(2, Timestamp.valueOf(this.dateHeureArrivee));
-            stmt.setInt(3, this.idHotel);
-            stmt.setString(4, this.idClient);
+            stmt.setInt(1, nbPassager);
+            stmt.setTimestamp(2, dateHeureArrivee);
+            stmt.setInt(3, hotel.getIdHotel());
+            stmt.setString(4, idClient);
 
             stmt.executeUpdate();
 
             ResultSet rs = stmt.getGeneratedKeys();
             if (rs.next()) {
-                this.idReservation = rs.getInt(1);
+                idReservation = rs.getInt(1);
             }
         }
     }
 
     public static List<Reservation> findAll() throws SQLException {
-        List<Reservation> reservations = new ArrayList<>();
-        String sql = "SELECT * FROM reservation";
+        List<Reservation> list = new ArrayList<>();
+
+        String sql = """
+            SELECT r.*, h.libelle, h.distance
+            FROM reservation r
+            JOIN hotel h ON r.id_hotel = h.id_hotel
+        """;
 
         try (Connection conn = DbConnection.getInstance().getConnection();
              Statement stmt = conn.createStatement();
              ResultSet rs = stmt.executeQuery(sql)) {
 
             while (rs.next()) {
+                Hotel hotel = new Hotel(
+                        rs.getInt("id_hotel"),
+                        rs.getString("libelle"),
+                        rs.getDouble("distance")
+                );
+
                 Reservation r = new Reservation();
-                r.setIdReservation(rs.getInt("id_reservation"));
-                r.setNbPassager(rs.getInt("nb_passager"));
-                r.setDateHeureArrivee(rs.getTimestamp("date_heure_arrivee").toLocalDateTime());
-                r.setIdHotel(rs.getInt("id_hotel"));
-                r.setIdClient(rs.getString("id_client"));
-                reservations.add(r);
+                r.idReservation = rs.getInt("id_reservation");
+                r.nbPassager = rs.getInt("nb_passager");
+                r.dateHeureArrivee = rs.getTimestamp("date_heure_arrivee");
+                r.hotel = hotel;
+                r.idClient = rs.getString("id_client");
+
+                list.add(r);
             }
         }
-        return reservations;
+        return list;
     }
-
 }
